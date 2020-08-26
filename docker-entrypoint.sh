262d731c9fc13adcf6d90706845b328975114861
Christoph <christoph.tittel@gmail.com>
2020-08-26 19:49:48 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 #set -x
 
 Xorg -noreset +extension GLX +extension RANDR +extension RENDER -logfile ./xdummy.log -config ./xorg.conf &
-
+x11vnc -bg -forever -nopw -quiet -shared -display WAIT$DISPLAY &
 if [ $@ ]; then
     exec $@
 fi
